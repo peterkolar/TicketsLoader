c1c76092a22a29264b6cc617f01885a1a4bb33b2
Peter Kolar <5erkolar@gmail.com>
2022-05-21 14:17:46 +0200

    add: circle rotation animation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { React, useEffect } from 'react';
-import Animated, { useSharedValue, useAnimatedProps, withTiming } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedProps, withTiming, withDelay, Easing } from 'react-native-reanimated';
 import Svg, { Circle } from 'react-native-svg';
 
 const { width, height } = Dimensions.get('window');
@@ -15,18 +15,24 @@ const CIRCLE_LENGTH = 2 * Math.PI * R;
 const STROKE_WIDTH = 20;
 
 const TIME_EXPAND_STROKE = 500;
+const TIME_ROTATION = 2500;
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 export default function App() {
   const strokeLength = useSharedValue(0);
+  const strokeRotation = useSharedValue(0);
 
   const animatedProps = useAnimatedProps(() => ({
     strokeDashoffset: CIRCLE_LENGTH - 0.001 - CIRCLE_LENGTH * strokeLength.value,
+    transform: [{translateX: width / 2}, {translateY: height / 2},
+      {rotate: strokeRotation.value},
+      {translateX: -width / 2}, {translateY: -height / 2}]
   }));
 
   useEffect(() => {
     strokeLength.value = withTiming(0.1, { duration: TIME_EXPAND_STROKE });
+    strokeRotation.value = withDelay(0, withTiming(3 * 2 * Math.PI, { duration: TIME_ROTATION, easing: Easing.inOut(Easing.cubic) }));
   }, []);
   
   return (
